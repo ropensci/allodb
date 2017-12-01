da20536b232b1b978a23904c79a38bbc26b36c09
maurolepore <maurolepore@gmail.com>
2017-12-01 10:22:51 -0500

    Fix links in doc.

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 
 #' Allometric equations form temperate sites.
 #'
-#' @source Erika B. Gonzalez ([GonzalezEB@@si.edu]).
+#' @source Erika B. Gonzalez (GonzalezEB@@si.edu).
 "allo_temperate"
 
 
@@ -13,14 +13,14 @@
 #' Data to from tropical forests.
 #'
 #' @family data from tropical forests
-#' @source Ervan Rutishauser ([er.rutishauser@@gmail.com]), received on Sat,
+#' @source Ervan Rutishauser (er.rutishauser@@gmail.com), received on Sat,
 #' Oct 28, 2017 at 4:05 AM.
 "ficus"
 
 #' Information on sites from tropical forests.
 #'
 #' @family data from tropical forests
-#' @source Ervan Rutishauser ([er.rutishauser@@gmail.com]), received on Sat,
+#' @source Ervan Rutishauser (er.rutishauser@@gmail.com), received on Sat,
 #' Oct 28, 2017 at 4:05 AM.
 "site.info"
 
@@ -28,12 +28,12 @@
 #'
 #' @seealso [wsg_ctfs3]
 #' @family data from tropical forests
-#' @source Ervan Rutishauser ([er.rutishauser@@gmail.com]), received on Sat,
+#' @source Ervan Rutishauser (er.rutishauser@@gmail.com), received on Sat,
 #' Oct 28, 2017 at 4:05 AM.
 "WSG"
 
 #' Wood density data set from CTFS sites.
 #'
 #' @family data from tropical forests
-#' @source Suzanne ([Lao LAOZ@@si.edu]).
+#' @source Suzanne (Lao LAOZ@@si.edu).
 "wsg_ctfs3"
