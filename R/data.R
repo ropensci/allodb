a05f14ca5f349f875f7df435f76d929486a9bff5
maurolepore <maurolepore@gmail.com>
2018-03-01 14:04:51 -0500

    #33: Document datasets.

---
+++ R/data.R
@@ -0,0 +1,44 @@
+#' Tables of allometric equations and associated metadata.
+#'
+#' * `equations`: Table of allometric equations.
+#' * `equations_metadata`: Table of metadata of `equations`.
+#'
+"equations"
+
+#' @rdname equations
+"equations_metadata"
+
+
+
+#' Table of metadata associated to the table `missing_values`.
+#'
+"missing_values_metadata"
+
+
+
+#' Table of metadata associated to the table `references`.
+#'
+"references_metadata"
+
+
+#' Tables of sites and species and associated metadata.
+#'
+#' * `sitespecies`: Table of sites and species.
+#' * `sitespecies_metadata`: Table of metadata of `sitespecies`.
+#'
+"sitespecies"
+
+#' @name sitespecies
+"sitespecies_metadata"
+
+
+
+#' Tables of wood density and associated metadata.
+#'
+#' * `wsg`: Table of allometric equations.
+#' * `wsg_metadata`: Table of metadata of `wsg`.
+#'
+"wsg"
+
+#' @name wsg
+"wsg_metadata"
