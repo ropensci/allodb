ecf75848d32b55fd622a5b3e9b40199320964163
maurolepore <maurolepore@gmail.com>
2018-10-18 15:52:05 -0400

    Update data.

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,63 +1,55 @@
 context("test-data.R")
 
+# # UPDATING EXPORTED DATA AND REGRESSION TESTS:
+# use_updated_data()
+
+# * Update test references: Temporarily change default `update = T`
+expect_output <- function(object, file, update = FALSE) {
+  expect_known_output(object, file, update = update, print = TRUE)
+}
+
+
+
 test_that("Exported datasets match known output", {
-  expect_known_output(
+  expect_output(
     as.data.frame(allodb::equations, stringsAsFactors = FALSE),
-    "ref-equations",
-    print = TRUE,
-    update = FALSE
+    "ref-equations"
   )
-  expect_known_output(
+  expect_output(
     as.data.frame(allodb::equations_metadata, stringsAsFactors = FALSE),
-    "ref-equations_metadata",
-    print = TRUE,
-    update = FALSE
+    "ref-equations_metadata"
   )
 
-  expect_known_output(
+  expect_output(
     as.data.frame(allodb::missing_values_metadata, stringsAsFactors = FALSE),
-    "ref-missing_values_metadata",
-    print = TRUE,
-    update = FALSE
+    "ref-missing_values_metadata"
   )
 
-  expect_known_output(
+  expect_output(
     as.data.frame(allodb::references_metadata, stringsAsFactors = FALSE),
-    "ref-references_metadata",
-    print = TRUE,
-    update = FALSE
+    "ref-references_metadata"
   )
 
-  expect_known_output(
+  expect_output(
     as.data.frame(allodb::sitespecies, stringsAsFactors = FALSE),
-    "ref-sitespecies",
-    print = TRUE,
-    update = FALSE
+    "ref-sitespecies"
   )
-  expect_known_output(
+  expect_output(
     as.data.frame(allodb::sitespecies_metadata, stringsAsFactors = FALSE),
-    "ref-sitespecies_metadata",
-    print = TRUE,
-    update = FALSE
+    "ref-sitespecies_metadata"
   )
 
-  expect_known_output(
+  expect_output(
     as.data.frame(allodb::sites_info, stringsAsFactors = FALSE),
-    "ref-sites_info",
-    print = TRUE,
-    update = FALSE
+    "ref-sites_info"
   )
 
-  expect_known_output(
+  expect_output(
     as.data.frame(allodb::wsg, stringsAsFactors = FALSE),
-    "ref-wsg",
-    print = TRUE,
-    update = FALSE
+    "ref-wsg"
   )
-  expect_known_output(
+  expect_output(
     as.data.frame(allodb::wsg_metadata, stringsAsFactors = FALSE),
-    "ref-wsg_metadata",
-    print = TRUE,
-    update = FALSE
+    "ref-wsg_metadata"
   )
 })
