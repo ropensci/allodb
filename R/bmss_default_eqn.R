5769845b664ae8a16ab3e62efd4a072cc9d6d2b4
maurolepore <maurolepore@gmail.com>
2018-11-08 17:26:35 -0500

    New bmss_cns().

--- R/bmss_default_eqn.R
+++ R/bmss_default_eqn.R
@@ -1,17 +1,27 @@
-#' Structure equaitons from __allodb__ as a __bmss__ default-equations table.
+#' Structure equations from __allodb__ as a __bmss__ default-equations table.
 #'
-#' @param .data An object of class 'allodb_eqn' built with [allodb::master()].
+#' This function restructures an equations-table from __allodb__ with columns as
+#' in [allodb_eqn_crucial()] (e.g. [allodb::master()]). It transforms its input
+#' into a default-equations table for __bmss__. Now this function is very
+#' strict and intrusive:
+#' * It drops problematic equations that can't be evaluated.
+#' * It adds and remove columns.
+#' * It renames columns.
+#' * It transforms text-values to lowercase to simplify matching.
+#' * It formats equations using [formatR::tidy_source()].
+#' * It drops missing values.
+#'
+#' @param .data [allodb::master()] or similar.
 #'
 #' @return A dataframe.
 #' @export
 #'
 #' @examples
-#' bmss_default_eqn()
+#' bmss_default_eqn(allodb::master())
 bmss_default_eqn <- function(.data) {
-  crucial <- c("site", "species", "equation_allometry", "allometry_specificity")
-  check_crucial_names(.data, crucial)
+  check_crucial_names(.data, allodb_eqn_crucial())
 
-  good <- .data[!.data$equation_id %in% .bad_eqn_id , crucial]
+  good <- .data[!.data$equation_id %in% .bad_eqn_id , allodb_eqn_crucial()]
   good %>%
     dplyr::mutate(
       eqn_source = "default",
@@ -22,15 +32,73 @@ bmss_default_eqn <- function(.data) {
       sp = .data$species,
       eqn_type = .data$allometry_specificity
       ) %>%
+    # Recover missing values represented as the literal "NA"
+    purrr::modify_if(is.character, readr::parse_character) %>%
     # Make it easier to find values (all lowercase)
     purrr::modify_if(is.character, tolower) %>%
     # Order
     dplyr::select(bmss_default_vars()) %>%
+    dplyr::filter(complete.cases(.)) %>%
     unique()
 }
 
-bmss_default_vars <- function() c("site", "sp", "eqn", "eqn_source", "eqn_type")
+bmss_default_vars <- function() {
+  c("site", "sp", "eqn", "eqn_source", "eqn_type")
+}
 
 format_equations <- function(eqn) {
   purrr::quietly(formatR::tidy_source)(text = eqn)$result$text.tidy
 }
+
+
+
+#' Create a census table of the kind useful in __bmss__.
+#'
+#' @param census A ForestGEO-like census-dataframe.
+#' @param species A ForestGEO-like species-dataframe.
+#' @param site The name of the site. One of `allodb::sites_info$site`.
+#'
+#' @return A dataframe.
+#' @export
+#'
+#' @examples
+#' bmss_cns(allodb::scbi_tree1, allodb::scbi_species, site = "scbi")
+bmss_cns <- function(census, species, site) {
+  .census <- rlang::set_names(census, tolower)
+  .species <- rlang::set_names(species, tolower)
+  .site <- tolower(site)
+  check_bms_cns(.census, .species, .site)
+
+  all <- dplyr::left_join(.census, .species, by = "sp")
+  all$sp <- tolower(all$latin)
+  all$site <- .site
+  all[c("site", "sp", "dbh")]
+}
+
+check_bms_cns <- function(census, species, site) {
+  stopifnot(
+    is.data.frame(census),
+    is.data.frame(species),
+    is.character(site),
+    length(site) == 1
+  )
+  check_crucial_names(census, c("sp", "dbh"))
+  check_crucial_names(species, "sp")
+}
+
+
+
+#' Crucial columns form __allodb__ equations-table used by `bmss_default_eqn()`.
+#'
+#' @return A string.
+#' @export
+#'
+#' @examples
+#' allodb_eqn_crucial()
+allodb_eqn_crucial <- function() {
+  c("site", "species", "equation_allometry", "allometry_specificity")
+}
+
+
+
+
--- tests/testthat/test-bmss_default_eqn.R
+++ tests/testthat/test-bmss_default_eqn.R
@@ -1,10 +1,15 @@
 context("bmss_default_eqn")
 
 test_that("outputs the expected data structure", {
-  allodb_eqn <- master() %>%
-    drop_bad_equations() %>%
-    pick_equations("species")
-
-  out <- bmss_default_eqn(allodb_eqn)
+  out <- bmss_default_eqn(allodb::master())
   expect_named(out, names(bmss::toy_default_eqn))
+})
+
+
+
+context("bmss_cns")
+
+test_that("outputs the expected data structure", {
+  out <- bmss_cns(allodb::scbi_tree1, allodb::scbi_species, "scbi")
+  expect_named(out, names(bmss::user_data))
 })
