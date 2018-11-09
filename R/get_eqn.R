7570e11071ccad876185fd9682910f72caf8a936
maurolepore <maurolepore@gmail.com>
2018-11-09 14:57:16 -0500

    New get_eqn().

---
+++ R/get_eqn.R
@@ -0,0 +1,34 @@
+#' Get default equations of each type.
+#'
+#' @param dbh_species A dataframe as those created with [bmss_cns()].
+#'
+#' @return A nested dataframe with each row containing the data of an equation
+#'   type.
+#' @export
+#'
+#' @examples
+#' dbh_species <- bmss_cns(dbh_sp, species, site)
+#' get_eqn(dbh_species)
+get_eqn <- function(dbh_species) {
+  default_eqn <- allodb::default_eqn
+  type_data <- default_eqn %>%
+    dplyr::filter(!is.na(.data$eqn_type)) %>%
+    dplyr::group_by(.data$eqn_type) %>%
+    tidyr::nest() %>%
+    dplyr::mutate(data = purrr::map(data, ~get_this_eqn(.x, dbh_species)))
+
+  add_eqn_type(type_data)
+}
+
+get_this_eqn <- function(.type, dbh_species) {
+  dplyr::inner_join(dbh_species, .type, by = c("sp", "site")) %>%
+    dplyr::filter(!is.na(.data$dbh), !is.na(.data$eqn))
+}
+
+add_eqn_type <- function(type_data) {
+  types <- type_data$eqn_type
+  dplyr::mutate(
+    type_data,
+    data = purrr::map2(data, types, ~tibble::add_column(.x, eqn_type = .y))
+  )
+}
---
+++ tests/testthat/test-get_eqn.R
@@ -0,0 +1,3 @@
+context("get_eqn")
+
+
