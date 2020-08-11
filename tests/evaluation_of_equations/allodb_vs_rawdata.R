ba7d2de3b0f0cb370bb4b44531be391436819d8b
Erika Gonzalez-Akre <gonzalezeb@si.edu>
2020-08-11 11:50:32 -0400

    Update allodb_vs_rawdata.R

--- tests/evaluation_of_equations/allodb_vs_rawdata.R
+++ tests/evaluation_of_equations/allodb_vs_rawdata.R
@@ -1,6 +1,12 @@
 #comparing allodb predicted data against raw data (vaidation data).
 
 library(readxl)
+library(kgc)
+library(allodb)
+library(ggplot2)
+library(ggpubr)
+library(plotly)
+
 valdata <-read_excel("tests/evaluation_of_equations/1-Validation data.xlsx")
 
 #add Koppen zones to df (just in case)
@@ -13,10 +19,23 @@ valdata <- data.frame(valdata,ClimateZ=LookupCZ(valdata))
 valdata$agb_allodb = get_biomass(dbh=valdata$DBH,
                               species = valdata$Species,
                               genus= valdata$Genus,
-                              coords = cbind(valdata$Longitude, valdata$Latitude)/1000)
+                              coords = c(31.083, 67.650))
+                              #cbind(valdata$Longitude, valdata$Latitude)/1000)
 
-#I get the same warning 20 times and not agb values
+#cbind not working! I get the same warning 20 times and not agb values..
 warnings()
 
-plot (valdata$Ptot, valdata$agb_allodb)
+#plot results
+val = ggplot(valdata, aes(x = Ptot, y = agb_allodb)) +
+  geom_abline(slope=1, intercept=0, lty=2) +
+  geom_point(size=1)
 
+#Change label of axis
+val
+val +  xlab("True AGB (kg)") + ylab("Predicted AGB (kg)")
+
+#check values by moving mouse on graph
+ggplotly(val)
+
+sum(valdata$Ptot)
+sum(valdata$agb_allodb)
