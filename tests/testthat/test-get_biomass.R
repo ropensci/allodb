bfb18d1f8e5a12224f8a3072b57637f5dcd6163d
Camille Piponiot <camille.piponiot@gmail.com>
2021-01-04 18:11:08 +0100

    more explicit test-get_biomass file

--- tests/testthat/test-get_biomass.R
+++ tests/testthat/test-get_biomass.R
@@ -1,10 +1,16 @@
-context("Estimate aboveground biomass")
+context("Test all functionalities of the get_biomass function that estimates aboveground biomass")
 library(allodb)
 
-test_that("get_biomass works and provides reasonable outputs", {
-  expect_equal(get_biomass(dbh = 20, genus = "Quercus", species = "alba", coords = c(-78, 40)), 240, tolerance = 50)
-  expect_equal(get_biomass(dbh = 20, genus = "Quercus", coords = c(-78, 40)), 240, tolerance = 50)
-  expect_equal(get_biomass(dbh = 20, genus = "Quercus", coords = c(-78, 40), wna = 0.01, w95 = 1000), 240, tolerance = 50)
+test_that("get_biomass works with genus and species information and provides reasonable outputs", {
+  expect_equal(get_biomass(dbh = 20, genus = "Quercus", species = "alba", coords = c(-78, 40)),
+               240, tolerance = 50)
+  expect_equal(get_biomass(dbh = 20, genus = "Quercus", coords = c(-78, 40)),
+               240, tolerance = 50)
+})
+
+test_that("weighting parameters can be easily changed without changing results dramatically", {
+  expect_equal(get_biomass(dbh = 20, genus = "Quercus", coords = c(-78, 40), wna = 0.01, w95 = 1000),
+               240, tolerance = 50)
 })
 
 test_that("get_biomass returns zero when dbh = 0 and NA when dbh = NA or dbh < 0", {
@@ -19,21 +25,28 @@ test_that("get_biomass accepts new equation table", {
 })
 
 test_that("get_biomass can be used for several individuals and sites", {
-  expect_equal(get_biomass(dbh = c(20, 40, 10), genus = c("Quercus", "Acer", "Fagus"), coords = c(-78, 40)), c(240, 1500, 50), tolerance = 50)
-  expect_equal(get_biomass(dbh = c(20, 40, 10), genus = c("Quercus", "Acer", "Fagus"), coords = rbind(c(-78, 40), c(-78, 40), c(-85, 45))), c(240, 1500, 50), tolerance = 50)
+  inds_1site <- get_biomass(dbh = c(20, 40, 10),
+                            genus = c("Quercus", "Acer", "Fagus"),
+                            coords = c(-78, 40))
+  inds_2site <- get_biomass(dbh = c(20, 40, 10),
+                             genus = c("Quercus", "Acer", "Fagus"),
+                             coords = rbind(c(-78, 40),
+                                            c(-78, 40),
+                                            c(-85, 45)))
+  expect_equal(inds_1site, c(240, 1500, 50), tolerance = 50)
+  expect_equal(inds_2site, c(240, 1500, 50), tolerance = 50)
 })
-
 
 test_that("get_biomass gives stable results when run several times with the same inputs", {
-  expect_equal(get_biomass(dbh = 20, genus = "Quercus", coords = c(-78, 40)),
-               get_biomass(dbh = 20, genus = "Quercus", coords = c(-78, 40)),
-               tolerance = 1)
+  run_1 <- get_biomass(dbh = 20, genus = "Quercus", coords = c(-78, 40))
+  run_2 <- get_biomass(dbh = 20, genus = "Quercus", coords = c(-78, 40))
+  expect_equal(run_1, run_2, tolerance = 1)
 })
 
-test_that("Biomass estimates for one individual do not depend on other measurements.", {
-  expect_equal(get_biomass(dbh = c(20, 10), genus = c("Quercus", "Fagus"), coords = c(-78, 40))[1],
-               get_biomass(dbh = c(20, 50), genus = c("Quercus", "Fagus"), coords = c(-78, 40))[1],
-               tolerance = 1)
+test_that("biomass estimates for one individual do not depend on other measurements", {
+  agb_alone <- get_biomass(dbh = 20, genus = "Quercus", coords = c(-78, 40))
+  agb_withfagus <- get_biomass(dbh = c(20, 10), genus = c("Quercus", "Fagus"), coords = c(-78, 40))[1]
+  expect_equal(agb_alone, agb_withfagus, tolerance = 1)
 })
 
 test_that("get_biomass gives error message when inputs are not the same length", {
