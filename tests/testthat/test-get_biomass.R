5d164bec4322e652f47e67b54ea1980fb3cd5aaf
Erika Gonzalez-Akre <gonzalezeb@si.edu>
2021-02-16 22:00:00 -0500

    Shortened lines

--- tests/testthat/test-get_biomass.R
+++ tests/testthat/test-get_biomass.R
@@ -1,6 +1,7 @@
 library(allodb)
 
-test_that("get_biomass works with genus and species information and provides reasonable outputs",
+test_that("get_biomass works with genus and species
+          information and provides reasonable outputs",
           {
             expect_equal(get_biomass(
               dbh = 20,
@@ -19,7 +20,8 @@ test_that("get_biomass works with genus and species information and provides rea
             tolerance = 50)
           })
 
-test_that("weighting parameters can be easily changed without changing results dramatically",
+test_that("weighting parameters can be easily changed
+          without changing results dramatically",
           {
             expect_equal(
               get_biomass(
@@ -34,7 +36,8 @@ test_that("weighting parameters can be easily changed without changing results d
             )
           })
 
-test_that("get_biomass returns zero when dbh = 0 and NA when dbh = NA or dbh < 0", {
+test_that("get_biomass returns zero when dbh = 0 and NA when
+          dbh = NA or dbh < 0", {
   expect_equal(get_biomass(
     dbh = 0,
     genus = "Quercus",
@@ -84,7 +87,8 @@ test_that("get_biomass can be used for several individuals and sites", {
   expect_equal(inds_2site, c(240, 1500, 50), tolerance = 50)
 })
 
-test_that("get_biomass gives stable results when run several times with the same inputs",
+test_that("get_biomass gives stable results when run several
+          times with the same inputs",
           {
             run_1 <-
               get_biomass(dbh = 20,
@@ -97,7 +101,8 @@ test_that("get_biomass gives stable results when run several times with the same
             expect_equal(run_1, run_2)
           })
 
-test_that("biomass estimates for one individual do not depend on other measurements",
+test_that("biomass estimates for one individual do not
+          depend on other measurements",
           {
             agb_alone <-
               get_biomass(dbh = 20,
@@ -112,7 +117,8 @@ test_that("biomass estimates for one individual do not depend on other measureme
             expect_equal(agb_alone, agb_withfagus)
           })
 
-test_that("get_biomass gives error message when inputs are not the same length",
+test_that("get_biomass gives error message when inputs are
+          not the same length",
           {
             expect_error(get_biomass(
               dbh = c(20, 10, 30),
