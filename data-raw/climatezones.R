6eae3cb173a684ad486b9acae950b2f5b2ee94d4
Mauro Lepore <maurolepore@gmail.com>
2021-10-06 07:30:16 -0600

    Cite kgc properly
    Relates to #200
    
    This commit adds the output of
    
        citation("kgc")

--- data-raw/climatezones.R
+++ data-raw/climatezones.R
@@ -1,3 +1,19 @@
-# kgc 1.0.0.2
-climatezones <- climatezones
+# To cite package ‘kgc’ in publications use:
+#
+#   Chelsey Bryant, Nicholas R. Wheeler, Franz
+#   Rubel and Roger H. French (2017). kgc:
+#   Koeppen-Geiger Climatic Zones. R package
+#   version 1.0.0.2.
+#   https://CRAN.R-project.org/package=kgc
+#
+# A BibTeX entry for LaTeX users is
+#
+#   @Manual{,
+#     title = {kgc: Koeppen-Geiger Climatic Zones},
+#     author = {Chelsey Bryant and Nicholas R. Wheeler and Franz Rubel and Roger H. French},
+#     year = {2017},
+#     note = {R package version 1.0.0.2},
+#     url = {https://CRAN.R-project.org/package=kgc},
+#   }
+climatezones <- kgc::climatezones
 use_data(climatezones, internal = TRUE, overwrite = TRUE)
