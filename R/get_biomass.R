59546660e7d920e6040fab8106efa2d87413bdfa
Mauro Lepore <maurolepore@gmail.com>
2021-10-19 12:59:43 -0600

    Spellcheck

--- R/get_biomass.R
+++ R/get_biomass.R
@@ -41,7 +41,7 @@
 #' the target location, and 3) its taxonomic similarity with the target taxon
 #' (see documentation of the `weight_allom()` function). The final weight
 #' attributed to each equation is the product of those three weights.
-#' Equations are then resampled with the`resample_agb()` funtion: the number
+#' Equations are then resampled with the`resample_agb()` function: the number
 #' of samples per equation is proportional to its weight, and the total number
 #' of samples is provided by the argument `nres`. The resampling is done by
 #' drawing DBH values from a uniform distribution on the DBH range of the
